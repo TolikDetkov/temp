8725d4f5c1f626cd4c3c497ab6f107e75a3363db
Анатолий <anatoly.detkov@mail.ru>
2019-11-10 21:15:07 +0100

    second commit

--- endPro/src/Main.java
+++ endPro/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("second commit");
+        System.out.println("third commit");
 
     }
 }
